8fc57a68038230bbe2dc7b1edbc032fa4272ea61
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-05-27 16:48:08 +0100

    1.0.4

---
+++ Example/Tests/KWSTokenTests.m
@@ -0,0 +1,46 @@
+//
+//  KWSTokenTests.m
+//  KWSiOSSDKObjC
+//
+//  Created by Gabriel Coman on 27/05/2016.
+//  Copyright © 2016 Gabriel Coman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KWSTokenTests : XCTestCase
+
+@end
+
+@implementation KWSTokenTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void) testIncompleteToken {
+    // given
+    NSString *token = @"020-10-21021kjjksajklas";
+    
+    // when
+    
+    // then
+}
+
+- (void) testValidToken {
+    
+}
+
+- (void) testInvalidToken {
+    
+}
+
+
+
+@end
