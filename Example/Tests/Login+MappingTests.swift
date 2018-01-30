7637c1afd040833ae3c8163a0b111b7fa1bd810f
Guilherme Mota <guilherme.mota@superawesome.tv>
2018-01-30 15:40:24 +0000

    Added TestUtils. Updated pod spec.

--- Example/Tests/Login+MappingTests.swift
+++ Example/Tests/Login+MappingTests.swift
@@ -7,6 +7,10 @@
 //
 
 import XCTest
+import Nimble
+import Decodable
+import protocol Decodable.Decodable
+import KWSiOSSDKObjC
 
 class Login_MappingTests: XCTestCase {
     
@@ -20,16 +24,14 @@ class Login_MappingTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testLoginMapping(){
+        
+        var JSON: Any?
+        JSON = try? fixtureW
+        
+        
     }
+   
     
 }
