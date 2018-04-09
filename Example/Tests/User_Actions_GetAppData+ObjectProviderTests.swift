dfe8541a2b321c407086dcb2a10bd7d48d9a7e88
Guilherme Mota <guilherme.mota@superawesome.tv>
2018-04-09 17:51:59 +0100

    Update spaces.

--- Example/Tests/User_Actions_GetAppData+ObjectProviderTests.swift
+++ Example/Tests/User_Actions_GetAppData+ObjectProviderTests.swift
@@ -27,8 +27,7 @@ class User_Actions_GetAppData_ObjectProviderTests: XCTestCase {
     private var badToken: String = "bad_token"
     
     private var goodAppId: Int = 1
-    private var badAppId: Int = -1
-    
+    private var badAppId: Int = -1    
     
     override func setUp() {
         super.setUp()
