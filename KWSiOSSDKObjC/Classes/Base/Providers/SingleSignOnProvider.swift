c73b6d86dc92cde08599a45751e2f62c2afe7864
Guilherme Mota <guilherme.mota@superawesome.tv>
2018-04-15 18:54:32 +0100

    Update

--- KWSiOSSDKObjC/Classes/Base/Providers/SingleSignOnProvider.swift
+++ KWSiOSSDKObjC/Classes/Base/Providers/SingleSignOnProvider.swift
@@ -10,7 +10,7 @@ import SAMobileBase
 import SAProtobufs
 
 public class SingleSignOnProvider: NSObject, SingleSignOnServiceProtocol {
-  
+    
     var environment: KWSNetworkEnvironment
     
     public init(environment: KWSNetworkEnvironment) {
@@ -26,6 +26,53 @@ public class SingleSignOnProvider: NSObject, SingleSignOnServiceProtocol {
         print("OAuth data codeChallenge:\(oAuthDataClass.codeChallenge)\nOAuth data code verifier: \(oAuthDataClass.codeVerifier)\nOAuth data code method: \(oAuthDataClass.codeChallengeMethod)")
         
         
+    }
+    
+    private func getAccessToken(environment: KWSNetworkEnvironment,
+                                authCode: String,
+                                codeVerifier: String, completionHandler: @escaping (LoggedUser?, Error?) -> ()) {
+        
+        let getOAuthTokenNetworkRequest = OAuthUserTokenRequest(environment: environment,
+                                                                clientID: environment.appID,
+                                                                authCode: authCode,
+                                                                codeVerifier: codeVerifier,
+                                                                clientSecret: environment.mobileKey)
+        
+        let parseTask = ParseJsonTask<LoginAuthResponse>()
+        let networkTask = NetworkTask()
+        
+        let future = networkTask
+            .execute(input: getOAuthTokenNetworkRequest)
+            .map { (result: Result<String>) -> Result <LoginAuthResponse> in
+                return result.then(parseTask.execute)
+        }
+        
+        future.onResult { result in
+            
+            switch result {
+            case .success(let value):
+                
+                let token = value.token
+                
+                let base64Task = ParseBase64Task()
+                let parseTask = ParseJsonTask<TokenData>()
+                let tokenResult = base64Task.execute(input: token).then(parseTask.execute)
+                
+                switch tokenResult {
+                case .success(let tokenData):
+                    if let userId = tokenData.userId {
+                        let user = LoggedUser(token: token, tokenData: tokenData, id: userId)
+                        completionHandler(user, nil)
+                    }
+                case .error(let error):
+                    let mappedError = Provider().mapErrorResponse(error: error)
+                    completionHandler(nil, mappedError)
+                }
+            case .error(let error):
+                let mappedError = Provider().mapErrorResponse(error: error)
+                completionHandler(nil, mappedError)
+            }
+        }
     }
     
 }
