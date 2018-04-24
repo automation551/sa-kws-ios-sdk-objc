0d5976b76303bfe5f3d4f4c583d1743d3ad5133e
Guilherme Mota <guilherme.mota@superawesome.tv>
2018-04-24 10:59:22 +0100

    Updated 2 step tests. Re arranged folders in tests

--- Example/Tests/AuthServiceTests.swift
+++ Example/Tests/AuthServiceTests.swift
@@ -67,10 +67,259 @@ class AuthServiceTests: XCTestCase {
         
         waitUntil { done in
         
-            self.service.createUser(username: "aa", password: "pwd", timeZone: nil, dateOfBirth: "2012-02-02", country: "GB", parentEmail: "mail@mail.com") { (model, error) in
+            self.service.createUser(username: "username123", password: "testpwd", timeZone: nil, dateOfBirth: "2012-02-02", country: "GB", parentEmail: "parent.mail@mail.com") { (model, error) in
                 
                 expect(model).toNot(beNil())
+                expect(model?.token).to(equal("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjUxMjEsImFwcElkIjozNTgsImNsaWVudElkIjoia3dzLXNkay10ZXN0aW5nIiwic2NvcGUiOiJ1c2VyIiwiaWF0IjoxNTI0NDgzMzQ5LCJleHAiOjE4Mzk4NDMzNDksImlzcyI6InN1cGVyYXdlc29tZSJ9.go6JGvx9TEdM963QwexhLmvjNQp-u1qlwQ9-o_N-KNBRwLtI3EJJOGo8DAv0gEt1HPJ4VYKHxlEk3pisaTLzOFQ0jCIK7_vqXWKNFr9Tj7DEUbh8Nq1I47JtCgDyfgCCwoMvGYcv3nUEqg0b0SXCrVYdyQvO1UWDNurAcT8Cfts8awkKcMETXR1-uDr1-EnyDX21IjWu9U7K9seclePYcdwbLVYCly81PM3ZTwfisutoBofDoGfnc33exymqVHB8U-_csvs11hiUVw6KAGTsD2SEI8k1sedUDq-GPXSMTPDrdFB7W4CGXEJsnKD54g8CFGnTKHvw2Oea-D48-KN-_w"))
+                expect(model?.id).to(equal(170))
                 expect(error).to(beNil())
+                
+                done()
+            }
+        }
+    }
+    
+    func test_CreateUser_BadUsername_Response(){
+        
+        let TEMP_ACCESS_TOKEN: Any? = try? fixtureWithName(name: "temp_access_token_success_response")
+        let CREATE_USER: Any? = try? fixtureWithName(name: "create_user_bad_username_response")
+        
+        let tempAccessURI = "https://localhost:8080/oauth/token"
+        
+        let appID = 358
+        let createURI = "https://localhost:8080/v1/apps/\(appID)/users?access_token=\(goodToken)"
+        
+        stub(http(.post, uri: tempAccessURI), json(TEMP_ACCESS_TOKEN!))
+        stub(http(.post, uri: createURI), json(CREATE_USER!, status: 401))
+        
+        waitUntil { done in
+            
+            self.service.createUser(username: "a1", password: "testpwd", timeZone: nil, dateOfBirth: "2012-02-02", country: "GB", parentEmail: "parent.mail@mail.com") { (model, error) in
+                
+                expect(model).to(beNil())
+                expect(error).toNot(beNil())
+                
+                expect((error as! ErrorWrapper).code).to(equal(5))
+                expect((error as! ErrorWrapper).codeMeaning).to(equal("validation"))
+                expect((error as! ErrorWrapper).message).to(equal("child \"username\" fails because [\"username\" length must be at least 3 characters long]"))
+                expect((error as! ErrorWrapper).invalid?.username?.code).to(equal(7))
+                expect((error as! ErrorWrapper).invalid?.username?.codeMeaning).to(equal("invalidValue"))
+                expect((error as! ErrorWrapper).invalid?.username?.message).to(equal("\"username\" length must be at least 3 characters long"))
+                
+                done()
+            }
+        }
+    }
+    
+    func test_CreateUser_Bad_Username_Conflict_Response(){
+        
+        let TEMP_ACCESS_TOKEN: Any? = try? fixtureWithName(name: "temp_access_token_success_response")
+        let CREATE_USER: Any? = try? fixtureWithName(name: "create_user_conflict_username_taken_response")
+        
+        let tempAccessURI = "https://localhost:8080/oauth/token"
+        
+        let appID = 358
+        let createURI = "https://localhost:8080/v1/apps/\(appID)/users?access_token=\(goodToken)"
+        
+        stub(http(.post, uri: tempAccessURI), json(TEMP_ACCESS_TOKEN!))
+        stub(http(.post, uri: createURI), json(CREATE_USER!, status: 401))
+        
+        waitUntil { done in
+            
+            self.service.createUser(username: "username123", password: "testpwd", timeZone: nil, dateOfBirth: "2012-02-02", country: "GB", parentEmail: "parent.mail@mail.com") { (model, error) in
+                
+                expect(model).to(beNil())
+                expect(error).toNot(beNil())
+                
+                expect((error as! ErrorWrapper).code).to(equal(10))
+                expect((error as! ErrorWrapper).codeMeaning).to(equal("conflict"))
+                expect((error as! ErrorWrapper).message).to(equal("username already taken"))
+                expect((error as! ErrorWrapper).invalid?.username?.code).to(equal(10))
+                expect((error as! ErrorWrapper).invalid?.username?.codeMeaning).to(equal("conflict"))
+                expect((error as! ErrorWrapper).invalid?.username?.message).to(equal("username already taken"))
+                
+                done()
+            }
+        }
+    }
+    
+    func test_CreateUser_Bad_Token_Response(){
+        
+        let TEMP_ACCESS_TOKEN: Any? = try? fixtureWithName(name: "temp_access_token_success_response")
+        let CREATE_USER: Any? = try? fixtureWithName(name: "generic_invalid_token_response")
+        
+        let tempAccessURI = "https://localhost:8080/oauth/token"
+        
+        let appID = 358
+        let createURI = "https://localhost:8080/v1/apps/\(appID)/users?access_token=\(goodToken)"
+        
+        stub(http(.post, uri: tempAccessURI), json(TEMP_ACCESS_TOKEN!))
+        stub(http(.post, uri: createURI), json(CREATE_USER!, status: 401))
+        
+        waitUntil { done in
+            
+            self.service.createUser(username: "username123", password: "testpwd", timeZone: nil, dateOfBirth: "2012-02-02", country: "GB", parentEmail: "parent.mail@mail.com") { (model, error) in
+                
+                expect(model).to(beNil())
+                expect(error).toNot(beNil())
+                
+                expect((error as! ErrorWrapper).errorCode).to(equal("invalid_token"))
+                expect((error as! ErrorWrapper).error).to(equal("The access token provided is invalid."))
+                
+                done()
+            }
+        }
+    }
+    
+    func test_CreateUser_Bad_Password_Response(){
+        
+        let TEMP_ACCESS_TOKEN: Any? = try? fixtureWithName(name: "temp_access_token_success_response")
+        let CREATE_USER: Any? = try? fixtureWithName(name: "create_user_bad_password_response")
+        
+        let tempAccessURI = "https://localhost:8080/oauth/token"
+        
+        let appID = 358
+        let createURI = "https://localhost:8080/v1/apps/\(appID)/users?access_token=\(goodToken)"
+        
+        stub(http(.post, uri: tempAccessURI), json(TEMP_ACCESS_TOKEN!))
+        stub(http(.post, uri: createURI), json(CREATE_USER!, status: 401))
+        
+        waitUntil { done in
+            
+            self.service.createUser(username: "username123", password: "testpwd", timeZone: nil, dateOfBirth: "2012-02-02", country: "GB", parentEmail: "parent.mail@mail.com") { (model, error) in
+                
+                expect(model).to(beNil())
+                expect(error).toNot(beNil())
+                
+                expect((error as! ErrorWrapper).code).to(equal(5))
+                expect((error as! ErrorWrapper).codeMeaning).to(equal("validation"))
+                expect((error as! ErrorWrapper).message).to(equal("child \"password\" fails because [\"password\" length must be at least 8 characters long]"))
+                expect((error as! ErrorWrapper).invalid?.password?.code).to(equal(7))
+                expect((error as! ErrorWrapper).invalid?.password?.codeMeaning).to(equal("invalidValue"))
+                expect((error as! ErrorWrapper).invalid?.password?.message).to(equal("\"password\" length must be at least 8 characters long"))
+                
+                done()
+            }
+        }
+    }
+    
+    func test_CreateUser_Bad_Date_Of_Birth_Response(){
+        
+        let TEMP_ACCESS_TOKEN: Any? = try? fixtureWithName(name: "temp_access_token_success_response")
+        let CREATE_USER: Any? = try? fixtureWithName(name: "create_user_bad_dob_response")
+        
+        let tempAccessURI = "https://localhost:8080/oauth/token"
+        
+        let appID = 358
+        let createURI = "https://localhost:8080/v1/apps/\(appID)/users?access_token=\(goodToken)"
+        
+        stub(http(.post, uri: tempAccessURI), json(TEMP_ACCESS_TOKEN!))
+        stub(http(.post, uri: createURI), json(CREATE_USER!, status: 401))
+        
+        waitUntil { done in
+            
+            self.service.createUser(username: "username123", password: "testpwd", timeZone: nil, dateOfBirth: "123456", country: "GB", parentEmail: "parent.mail@mail.com") { (model, error) in
+                
+                expect(model).to(beNil())
+                expect(error).toNot(beNil())
+                
+                expect((error as! ErrorWrapper).code).to(equal(5))
+                expect((error as! ErrorWrapper).codeMeaning).to(equal("validation"))
+                expect((error as! ErrorWrapper).message).to(equal("child \"dateOfBirth\" fails because [\"dateOfBirth\" with value \"a\" fails to match the required pattern: /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/]"))
+                expect((error as! ErrorWrapper).invalid?.dateOfBirth?.code).to(equal(7))
+                expect((error as! ErrorWrapper).invalid?.dateOfBirth?.codeMeaning).to(equal("invalidValue"))
+                expect((error as! ErrorWrapper).invalid?.dateOfBirth?.message).to(equal("\"dateOfBirth\" with value \"a\" fails to match the required pattern: /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/"))
+                
+                done()
+            }
+        }
+    }
+    
+    func test_CreateUser_Bad_Country_Response(){
+        
+        let TEMP_ACCESS_TOKEN: Any? = try? fixtureWithName(name: "temp_access_token_success_response")
+        let CREATE_USER: Any? = try? fixtureWithName(name: "create_user_bad_country_response")
+        
+        let tempAccessURI = "https://localhost:8080/oauth/token"
+        
+        let appID = 358
+        let createURI = "https://localhost:8080/v1/apps/\(appID)/users?access_token=\(goodToken)"
+        
+        stub(http(.post, uri: tempAccessURI), json(TEMP_ACCESS_TOKEN!))
+        stub(http(.post, uri: createURI), json(CREATE_USER!, status: 401))
+        
+        waitUntil { done in
+            
+            self.service.createUser(username: "username123", password: "testpwd", timeZone: nil, dateOfBirth: "123456", country: "GB", parentEmail: "parent.mail@mail.com") { (model, error) in
+                
+                expect(model).to(beNil())
+                expect(error).toNot(beNil())
+                
+                expect((error as! ErrorWrapper).code).to(equal(5))
+                expect((error as! ErrorWrapper).codeMeaning).to(equal("validation"))
+                expect((error as! ErrorWrapper).message).to(equal("child \"country\" fails because [\"country\" with value \"a\" fails to match the required pattern: /^[A-Z]{2}$/]"))
+                expect((error as! ErrorWrapper).invalid?.country?.code).to(equal(7))
+                expect((error as! ErrorWrapper).invalid?.country?.codeMeaning).to(equal("invalidValue"))
+                expect((error as! ErrorWrapper).invalid?.country?.message).to(equal("\"country\" with value \"a\" fails to match the required pattern: /^[A-Z]{2}$/"))
+                
+                done()
+            }
+        }
+    }
+    
+    func test_CreateUser_Bad_Parent_Email_Response(){
+        
+        let TEMP_ACCESS_TOKEN: Any? = try? fixtureWithName(name: "temp_access_token_success_response")
+        let CREATE_USER: Any? = try? fixtureWithName(name: "create_user_bad_email_response")
+        
+        let tempAccessURI = "https://localhost:8080/oauth/token"
+        
+        let appID = 358
+        let createURI = "https://localhost:8080/v1/apps/\(appID)/users?access_token=\(goodToken)"
+        
+        stub(http(.post, uri: tempAccessURI), json(TEMP_ACCESS_TOKEN!))
+        stub(http(.post, uri: createURI), json(CREATE_USER!, status: 401))
+        
+        waitUntil { done in
+            
+            self.service.createUser(username: "username123", password: "testpwd", timeZone: nil, dateOfBirth: "2012-02-02", country: "GB", parentEmail: "mail.com") { (model, error) in
+                
+                expect(model).to(beNil())
+                expect(error).toNot(beNil())
+                
+                expect((error as! ErrorWrapper).code).to(equal(5))
+                expect((error as! ErrorWrapper).codeMeaning).to(equal("validation"))
+                expect((error as! ErrorWrapper).message).to(equal("child \"parentEmail\" fails because [\"parentEmail\" must be a valid email]"))
+                expect((error as! ErrorWrapper).invalid?.parentEmail?.code).to(equal(7))
+                expect((error as! ErrorWrapper).invalid?.parentEmail?.codeMeaning).to(equal("invalidValue"))
+                expect((error as! ErrorWrapper).invalid?.parentEmail?.message).to(equal("\"parentEmail\" must be a valid email"))
+                done()
+            }
+        }
+    }
+    
+    func test_CreateUser_Not_Found_Response(){
+        
+        let TEMP_ACCESS_TOKEN: Any? = try? fixtureWithName(name: "temp_access_token_success_response")
+        let CREATE_USER: Any? = try? fixtureWithName(name: "generic_simpler_not_found_response")
+        
+        let tempAccessURI = "https://localhost:8080/oauth/token"
+        
+        let appID = 358
+        let createURI = "https://localhost:8080/v1/apps/\(appID)/users?access_token=\(goodToken)"
+        
+        stub(http(.post, uri: tempAccessURI), json(TEMP_ACCESS_TOKEN!))
+        stub(http(.post, uri: createURI), json(CREATE_USER!, status: 404))
+        
+        waitUntil { done in
+            
+            self.service.createUser(username: "username123", password: "testpwd", timeZone: nil, dateOfBirth: "2012-02-02", country: "GB", parentEmail: "parent.mail@mail.com") { (model, error) in
+                
+                expect(model).to(beNil())
+                expect(error).toNot(beNil())
+                
+                expect((error as! ErrorWrapper).code).to(equal(123))
+                expect((error as! ErrorWrapper).codeMeaning).to(equal("notFound"))
                 
                 done()
             }
@@ -99,7 +348,7 @@ class AuthServiceTests: XCTestCase {
                                     
                                     //then
                                     expect(loginResponse).toNot(beNil())
-                                    expect(loginResponse?.token).to(equal("good_token"))
+                                    expect(loginResponse?.token).to(equal("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjUwNzMsImFwcElkIjozNTgsImNsaWVudElkIjoia3dzLXNkay10ZXN0aW5nIiwic2NvcGUiOiJ1c2VyIiwiaWF0IjoxNTI0NTU4NzY3LCJleHAiOjE4Mzk5MTg3NjcsImlzcyI6InN1cGVyYXdlc29tZSJ9.Is_paGut_gq7Zo5r0eWcH4GI3BCA8-eFiFurrb080O0xY0GWo2SIIR5FKf4_iDNVy_LM0x4j5kzDowl5gOZ7O3-KelA9MO6mpADl6eyVA109Z3ij3uZZU_Y7HRoA8XFL_TgUq_I7ZcpiXFhf4kzUEVEstkaBpEJXOFfcr9SedG7HD6r94zNKVaRGbS1x9Y8eOCdQf1ainizl0MEjjU7LFjUXv4eOc9Vj57N9oVEY2_cqd0No5Sa0sPUZaEVSuRQDR78dhBuLJKttgovGFdJZj7YgLOmjPaFvqHkpuS50h2oh_YP-v-gzSFItRPWqtDOIyHSMLOvoOb8-FeptveBXsw"))
                                     
                                     expect(error).to(beNil())
                                     
--- Example/Tests/CreateUser+MappingTests.swift
+++ Example/Tests/CreateUser+MappingTests.swift
@@ -31,7 +31,7 @@ class CreateUserMappingTests: XCTestCase {
         
         expect(createUserResponse).toNot(beNil())
         expect(createUserResponse?.id).to(equal(170))
-        expect(createUserResponse?.token).to(equal("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjE3MCwiYXBwSWQiOjIsImNsaWVudElkIjoic3Rhbi10ZXN0Iiwic2NvcGUiOiJ1c2VyIiwiaWF0IjoxNTIzMjAzMzI3LCJleHAiOjE1MjMyODk3MjcsImlzcyI6InN1cGVyYXdlc29tZSJ9.uqBAxvmUoyq-o22_cEG2zNKLvRzjTdY7OQu24xzjZDI"))
+        expect(createUserResponse?.token).to(equal("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjUxMjEsImFwcElkIjozNTgsImNsaWVudElkIjoia3dzLXNkay10ZXN0aW5nIiwic2NvcGUiOiJ1c2VyIiwiaWF0IjoxNTI0NDgzMzQ5LCJleHAiOjE4Mzk4NDMzNDksImlzcyI6InN1cGVyYXdlc29tZSJ9.go6JGvx9TEdM963QwexhLmvjNQp-u1qlwQ9-o_N-KNBRwLtI3EJJOGo8DAv0gEt1HPJ4VYKHxlEk3pisaTLzOFQ0jCIK7_vqXWKNFr9Tj7DEUbh8Nq1I47JtCgDyfgCCwoMvGYcv3nUEqg0b0SXCrVYdyQvO1UWDNurAcT8Cfts8awkKcMETXR1-uDr1-EnyDX21IjWu9U7K9seclePYcdwbLVYCly81PM3ZTwfisutoBofDoGfnc33exymqVHB8U-_csvs11hiUVw6KAGTsD2SEI8k1sedUDq-GPXSMTPDrdFB7W4CGXEJsnKD54g8CFGnTKHvw2Oea-D48-KN-_w"))
     }
     
     func test_CreateUser_BadUsername_Response(){
@@ -49,7 +49,7 @@ class CreateUserMappingTests: XCTestCase {
         expect(errorResponse?.invalid?.username?.message).to(equal("\"username\" length must be at least 3 characters long"))
     }
     
-    func test_CreateUser_BadUsernameConflict_Response(){
+    func test_CreateUser_Bad_Username_Conflict_Response(){
         var JSON: Any?
         JSON = try? fixtureWithName(name:"create_user_conflict_username_taken_response")
         
@@ -64,7 +64,7 @@ class CreateUserMappingTests: XCTestCase {
         expect(errorResponse?.invalid?.username?.message).to(equal("username already taken"))
     }
     
-    func test_CreateUser_BadTokenResponse(){
+    func test_CreateUser_Bad_Token_Response(){
         var JSON: Any?
         JSON = try? fixtureWithName(name:"generic_invalid_token_response")
         
@@ -75,7 +75,7 @@ class CreateUserMappingTests: XCTestCase {
         expect(errorResponse?.error).to(equal("The access token provided is invalid."))
     }
     
-    func test_CreateUser_BadPassword_Response(){
+    func test_CreateUser_Bad_Password_Response(){
         var JSON: Any?
         JSON = try? fixtureWithName(name:"create_user_bad_password_response")
         
@@ -90,7 +90,7 @@ class CreateUserMappingTests: XCTestCase {
         expect(errorResponse?.invalid?.password?.message).to(equal("\"password\" length must be at least 8 characters long"))
     }
     
-    func test_CreateUser_BadDateOfBirth_Response(){
+    func test_CreateUser_Bad_Date_Of_Birth_Response(){
         var JSON: Any?
         JSON = try? fixtureWithName(name:"create_user_bad_dob_response")
         
@@ -105,7 +105,7 @@ class CreateUserMappingTests: XCTestCase {
         expect(errorResponse?.invalid?.dateOfBirth?.message).to(equal("\"dateOfBirth\" with value \"a\" fails to match the required pattern: /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/"))
     }
     
-    func test_CreateUser_BadCountry_Response(){
+    func test_CreateUser_Bad_Country_Response(){
         var JSON: Any?
         JSON = try? fixtureWithName(name:"create_user_bad_country_response")
         
@@ -120,7 +120,7 @@ class CreateUserMappingTests: XCTestCase {
         expect(errorResponse?.invalid?.country?.message).to(equal("\"country\" with value \"a\" fails to match the required pattern: /^[A-Z]{2}$/"))
     }
     
-    func test_CreateUser_BadParentEmail_Response(){
+    func test_CreateUser_Bad_Parent_Email_Response(){
         var JSON: Any?
         JSON = try? fixtureWithName(name:"create_user_bad_email_response")
         
@@ -135,7 +135,7 @@ class CreateUserMappingTests: XCTestCase {
         expect(errorResponse?.invalid?.parentEmail?.message).to(equal("\"parentEmail\" must be a valid email"))
     }
     
-    func test_CreateUser_NotFound_Response() {
+    func test_CreateUser_Not_Found_Response() {
         
         var JSON: Any?
         JSON = try? fixtureWithName(name:"generic_simpler_not_found_response")
