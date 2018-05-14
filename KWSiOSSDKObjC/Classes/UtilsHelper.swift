e03407987e6cbbca77d316ca5694f94a7c9b43d6
Guilherme Mota <guilherme.mota@superawesome.tv>
2018-05-14 16:33:26 +0100

    Added UtilsHelpers class to spit out a TokenData model, with the parsed result from the JWT token.

---
+++ KWSiOSSDKObjC/Classes/UtilsHelper.swift
@@ -0,0 +1,37 @@
+//
+//  UtilsHelper.swift
+//  KWSiOSSDKObjC
+//
+//  Created by Guilherme Mota on 14/05/2018.
+//
+
+import SAProtobufs
+import SAMobileBase
+
+public class UtilsHelper : NSObject{
+    
+    static let _singletonInstance = UtilsHelper()
+    
+    public override init() {
+        //This prevents others from using the default '()' initializer for this class.
+    }
+    
+    static public func getMetadataFromToke(token : String) -> TokenData?{
+        
+        let base64Task = ParseBase64Task()
+        let parseTask = ParseJsonTask<TokenData>()
+        
+        let tokenResult = base64Task.execute(input: token).then(parseTask.execute)
+        
+        switch tokenResult {
+        case .success(let tokenData):
+            return tokenData
+        case .error(_):
+            return nil
+        }
+    }
+    
+}
+
+
+
