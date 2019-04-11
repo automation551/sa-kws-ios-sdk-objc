ed6e29abc74ca3df48a80040ea2a1daa541cbc8a
Myles Eynon <myleseynon@hotmail.co.uk>
2019-04-11 10:57:31 +0100

    fix(Frameworks): Added framework compatibility to headers

--- Pod/Classes/Models/User/KWSUser.h
+++ Pod/Classes/Models/User/KWSUser.h
@@ -8,16 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-//#if defined(__has_include)
-//#if __has_include(<SAJsonParser/SAJsonParser.h>)
-//#import <SAJsonParser/SAJsonParser.h>
-//#else
-//#import "SAJsonParser.h"
-//#endif
-//#endif
-
+#if defined(__has_include)
+#if __has_include(<SAJsonParser/SAJsonParser.h>)
+#import <SAJsonParser/SAJsonParser.h>
+#else
 #import "SAJsonParser.h"
+#endif
+#if __has_include(<SAJsonParser/SABaseObject.h>)
+#import <SAJsonParser/SABaseObject.h>
+#else
 #import "SABaseObject.h"
+#endif
+#endif
 
 @class KWSPermissions;
 @class KWSAddress;
